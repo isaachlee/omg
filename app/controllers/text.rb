e32090e9266546322cf6df71b09cf7ab16e22b87
Jeremy <jp5486@gmail.com>
2015-12-17 15:57:44 -0800

    uncommented edits

--- app/controllers/text.rb
+++ app/controllers/text.rb
@@ -1,13 +1,12 @@
 
 post '/text' do
-  # text = params[:text].upcase
-  # text_array = text.split(" ")
-  # translated_array = []
-  # text_array.each do |word|
-  #   translated_array << translate_omg(word)
-  # end
-  # @translated_string = translated_array.join(" ")
+  text = params[:text].upcase
+  text_array = text.split(" ")
+  translated_array = []
+  text_array.each do |word|
+    translated_array << translate_omg(word)
+  end
+  @translated_string = translated_array.join(" ")
 
-  api_result = RestClient.get 'htt'
   erb :index
 end
